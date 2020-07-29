09fe04d70f5cdf593b708da8b1473a0d7d4f3975
Alex Matveev <amatveevdev9@gmail.com>
2020-07-29 18:48:38 +0300

    initial

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+import cn from 'classnames'
+import styles from './Chat.scss'
+
+function Chat(props) {
+
+  // const chatViewHeight = 10
+  // const cns = {
+  //   root: cn(
+  //     styles.container,
+  //     this.props.smallMode && styles.smallMode,
+  //   ),
+  // }
+
+  return null
+  // (
+    // <div className={ cns.root } ref={ this.chatContainer } onDragEnter={ this.showDropzone }>
+    //   <div className={ styles.adminPanel } ref={ this.adminPanel }>
+    //     {/* <Header editingDisabled={ editingDisabled }/> */}
+    //   </div>
+
+    //   <div className={ styles.chatView }  style={{ height: chatViewHeight }}>
+    //     {/* */}
+    //     {/* <Messages needScrollBottom={ scrollBottom } scrollBottom={ this.scrollBottom }/> */}
+    //   </div>
+
+    //   <div className={ styles.chatForm } ref={ this.chatForm }>
+    //     {/* { hasForm && <Form scrollBottom={ this.scrollBottom }/> } */}
+    //   </div>
+
+    // </div>
+  // )
+}
+
+
+export default Chat
