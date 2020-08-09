3ae347f3ea30ed6f3777d8152f0fc6b20e4829aa
Alex Matveev <amatveevdev9@gmail.com>
2020-08-10 00:04:51 +0300

    add navigation bar

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,44 @@
 
 import React from 'react'
 import axios from 'axios'
+import { NavLink } from 'react-router-dom'
 import Products from '../Products'
 import Customers from '../Products'
 import Managers from '../Products'
 import styles from './Dashboard.module.scss'
 
 function Dashboard() {
+
+  const renderNavigation = () => {
+    return (
+      <div className={styles.navigation}>
+        <div className={styles.links}>
+          <NavLink activeClassName={styles.active} to="/dashboard/admins">
+            {`💪`} <span>Админы</span>
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/dashboard/traits">
+            {`🤯 `} <span>Трейты</span>
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/dashboard/tasks">
+            {`📋 `} <span>Задачи</span>
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/dashboard/calls">
+            {`🤙`} <span>Звонки</span>
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/dashboard/controls">
+            {`🎛️`} <span>Щитовая</span>
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/dashboard/operational">
+            {`👩‍🏫`} <span>Операционка</span>
+          </NavLink>
+        </div>
+      </div>
+    )
+  }
+  
   return (
     <div className={styles.content}>
+      {renderNavigation()}
       <Products />
       <Customers />
       <Managers />
