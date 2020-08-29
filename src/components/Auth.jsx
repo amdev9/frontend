6709ee2720dd9b66a9ca9198f91024c184492159
Alex Matveev <amatveevdev9@gmail.com>
2020-08-29 14:33:35 +0300

    todo fix function component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 // import auth0 from "auth0-js";
 import axios from "axios"
 
@@ -13,105 +13,133 @@ import { AuthProvider } from "../context/authContext";
 //   responseType: "token id_token"
 // });
 
-class Auth extends Component {
-  constructor() {
-    super()
+function Auth(props) {
+  // constructor() {
+  //   super()
 
-    const tokens = localStorage.getItem('tokens') || ""
-    this.state = {
-      authenticated: false,
-      user: {
-        role: "visitor" // TODO: add user persistance https://joshwcomeau.com/react/persisting-react-state-in-localstorage/
-      },
-      accessToken: tokens,
-    };
+  const tokens = localStorage.getItem('tokens') || ""
+  const [authenticated, setAuthenticated] = useState(false)
+  const [user, setUser] = useState({
+    role: "visitor"
+  })
+  const [accessToken, setAccessToken] = useState(tokens)
 
-    
+  // this.state = {
+  //   authenticated: false,
+  //   user: {
+  //     role: "visitor" // TODO: add user persistance https://joshwcomeau.com/react/persisting-react-state-in-localstorage/
+  //   },
+  //   accessToken: tokens,
+  // };
 
-  }
+
+
+  // }
+
   
-  setTokens = (data) => {
+  function setTokens(data) {
     localStorage.setItem('tokens', JSON.stringify(data) || '');
     // setAuthTokens(data);
   }
-
-  initiateLogin = ({ email, password }, e) => {
-    e.preventDefault()
-    axios.post("http://localhost:3000/api/auth/login", {
-      email,
-      password
-    }).then(result => {
-      if (result.status === 200) {
-        this.setSession(result.data); 
-      } else {
-        // setIsError(true);
-      }
-    }).catch(e => {
-      // setIsError(true);
-    });
-
-  };
-
-  signUpHandler = (e, {email, password, firstName, lastName}) => {
-    e.preventDefault()
-    axios.post("http://localhost:3000/api/auth/register", {
-      email,
-      password,
-      lastName,
-      firstName,
-    }).then(result => {
-      if (result.status === 200) {
-        this.setSession(result.data); 
-        // setAuthTokens(result.data.accessToken);
-        // setLoggedIn(true);
-      } else {
-        // setIsError(true);
-      }
-    }).catch(e => {
-      // setIsError(true);
-    });
-  }
-
-
-  logout = () => {
-    this.setState({
-      authenticated: false,
-      user: {
-        role: "visitor"
-      },
-      accessToken: ""
-    });
-  };
-
-  setSession(data) {
-    const { user, token } = data
+  function setSession(data) {
+    const { user: userData, token } = data
     // const localUser = {
     //   id: user.id,
     //   email: user.email,
     //   role: user.role,
     // };
-    this.setState({
-      authenticated: true,
-      accessToken: token,
-      user,
-    });
-    this.setTokens(token)
+    // this.setState({
+    //   authenticated: true,
+    //   accessToken: token,
+    //   user,
+    // });
+
+    setAuthenticated(true)
+    setAccessToken(token)
+    setUser(userData)
+    setTokens(token)
+
   }
 
-  render() {
-    const authProviderValue = {
-      ...this.state,
-      initiateLogin: this.initiateLogin,
-      handleAuthentication: this.handleAuthentication,
-      signUpHandler: this.signUpHandler,
-      logout: this.logout
-    };
-    return (
-      <AuthProvider value={authProviderValue}>
-        {this.props.children}
-      </AuthProvider>
-    );
+  async function initiateLogin({ email, password }, e) {
+    e.preventDefault()
+    try {
+      const result = await axios.post("http://localhost:3000/api/auth/login", {
+        email,
+        password
+      })
+
+      if (result.status === 200) {
+        setSession(result.data);
+        // this.
+      } else {
+        // setIsError(true);
+      }
+
+    } catch (e) {
+      console.log('error axios ', e)
+    }
+
   }
+
+
+  async function signUpHandler(e, { email, password, firstName, lastName }) {
+    e.preventDefault()
+    const result = await axios.post("http://localhost:3000/api/auth/register", {
+      email,
+      password,
+      lastName,
+      firstName,
+    })
+    if (result.status === 200) {
+      setSession(result.data);
+      // this.
+      // setAuthTokens(result.data.accessToken);
+      // setLoggedIn(true);
+    } else {
+      // setIsError(true);
+    }
+  }
+
+
+  function logout() {
+
+    setAuthenticated(false)
+    setAccessToken('')
+    setUser({
+      role: "visitor"
+    })
+
+    // this.setState({
+    //   authenticated: false,
+      // user: {
+      //   role: "visitor"
+      // },
+    //   accessToken: ""
+    // });
+  };
+
+  
+
+  // render() {
+  const authProviderValue = {
+    // ...this.state,
+
+    authenticated,
+    user,
+    accessToken,
+    initiateLogin: initiateLogin,
+    // handleAuthentication: handleAuthentication,
+    signUpHandler: signUpHandler,
+    logout: logout
+  };
+  return (
+    <AuthProvider value={authProviderValue}>
+      {props.children}
+    </AuthProvider>
+  );
+  // }
 }
+
 
 export default Auth;
